3c10c514fcc294d034ab59551a4c8d7f9899bc91
vitaliybondarenko1 <talalay2021@bk.ru>
2022-12-25 17:55:58 +1000

    add r

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 int main() {
     int a = 12;
     int d = 11;
-    int c = 1;
+    int c = 111;
     char with[3] ={ 11, 12, 1};
-    printf("a + d + c = %d\n%s", a + d + c, with);
+    int r = 14;
+    printf("a + d + c = %d\n%s", a + d + c + r, with);
     return 0;
 }
